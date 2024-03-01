9f453e673006314a54c481ff3a3523b1d3eabc5c
53huzefa-zeenwala <huzefa.zeenwal@gmail.com>
2024-03-01 16:31:11 +0530

    Created Login and Data Page

--- EmployeeServerTwo/App_Start/Startup.Auth.cs
+++ EmployeeServerTwo/App_Start/Startup.Auth.cs
@@ -38,7 +38,7 @@ namespace EmployeeServerTwo
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
                 // In production mode set AllowInsecureHttp = false
                 AllowInsecureHttp = true
             };
